102e5990cf3d902a129c55f9ed1729b5ce97dcd9
Robin Meese <robin.meese@conciso.de>
2020-12-03 16:10:46 +0100

    initial commit vor p3

---
+++ praktikum3/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <assert.h>
+
+
+/**
+ * Main Function that calls all necessary methods for praktikum 3
+ * @return -1 when done with the operations
+ */
+int main()
+{
+
+    return -1;
+}
+
+
